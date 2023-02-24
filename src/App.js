b474830ebf268c5d3525adcc771df244053e0745
Excelemma <excelemma6@gmail.com>
2023-02-24 08:29:27 -0800

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+function App() {
+  return (
+    <div className="App">
+     <Product/>
+    </div>
+  );
+}
+
+export default App;
